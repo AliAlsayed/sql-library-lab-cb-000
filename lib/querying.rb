99289e7d16a509d5ffa30f5ef5f508de99af235c
Ali Alsayed <a_alsayed87@hotmail.com>
2018-12-12 08:25:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,32 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  """
+  select title, year from books where series_id = 1 order by year;
+  """
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  """
+  select name, motto from characters order by length(motto) DESC limit 1;
+  """
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  """
+  select species, count(species) from characters group by 1 order by 2 DESC limit 1;
+  """
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  """
+  select a.name, sg.name from authors a
+  join
+  series s
+  on a.id = s.author_id
+  join
+  subgenres sg
+  on s.subgenre_id = sg.id;
+  """
 end
 
 def select_series_title_with_most_human_characters
